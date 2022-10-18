bfaed5dd65a89b97e69568819307cc9f61f8b53d
Ana Paula Duarte <apduartte@gmail.com>
2022-10-18 17:54:47 -0300

    iniciando projeto java

---
+++ src/main/java/br/com/apduartte/App.java
@@ -0,0 +1,13 @@
+package br.com.apduartte;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
